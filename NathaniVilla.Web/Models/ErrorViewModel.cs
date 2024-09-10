94022173514011c054199d2069da451b852026ff
Samsuddin Asliwala <Samsupatel02@gmail.com>
2024-09-10 22:54:24 +0530

    Add project files.

---
+++ NathaniVilla.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NathaniVilla.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
