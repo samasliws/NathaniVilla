783c99ddfb9e18321cdfc7ec707860ef499ce274
Samsuddin Asliwala <Samsupatel02@gmail.com>
2024-09-14 21:52:42 +0530

    CreateformImplemented

---
+++ NathaniVilla.Web/Controllers/VillaController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NathaniVilla.Infrastructure.Data;
+
+namespace NathaniVilla.Web.Controllers
+{
+
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var villas = _db.Villas.ToList();
+            return View(villas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
