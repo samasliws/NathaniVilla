2eb47e6e91b3d6c1227d513afe97f594b145380c
Samsuddin Asliwala <Samsupatel02@gmail.com>
2024-09-15 21:13:36 +0530

    LayoutChanges

--- NathaniVilla.Web/Controllers/VillaController.cs
+++ NathaniVilla.Web/Controllers/VillaController.cs
@@ -14,6 +14,7 @@ namespace NathaniVilla.Web.Controllers
             _db = db;
         }
 
+        #region CRUD Operation :: Samsuddin Asliwala
         public IActionResult Index()
         {
             var villas = _db.Villas.ToList();
@@ -36,6 +37,7 @@ namespace NathaniVilla.Web.Controllers
             {
                 _db.Villas.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "The villa has been created successfully.";
                 return RedirectToAction("Index");
             }
             return View();
@@ -46,7 +48,7 @@ namespace NathaniVilla.Web.Controllers
             Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
             if (obj == null)
             {
-                return RedirectToAction("Error","Home");
+                return RedirectToAction("Error", "Home");
             }
             return View(obj);
         }
@@ -54,14 +56,43 @@ namespace NathaniVilla.Web.Controllers
         [HttpPost]
         public IActionResult Update(Villa obj)
         {
-            
+
             if (ModelState.IsValid)
             {
                 _db.Villas.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "The villa has been updated successfully.";
                 return RedirectToAction("Index");
             }
             return View();
         }
+
+        public IActionResult Delete(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {
+            Villa? objFromDb = _db.Villas.FirstOrDefault(u => u.Id == obj.Id);
+
+            if (objFromDb is not null)
+            {
+                _db.Villas.Remove(objFromDb);
+                _db.SaveChanges();
+                TempData["success"] = "The villa has been deleted successfully.";
+                return RedirectToAction("Index");
+            }
+            TempData["error"] = "The villa could not been deleted.";
+            return View();
+        }
+
+        #endregion
     }
 }
