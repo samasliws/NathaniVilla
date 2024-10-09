6c3c959dbbdab50da0feb664c15a87553ab25c6f
Samsuddin Asliwala <Samsupatel02@gmail.com>
2024-10-09 22:53:46 +0530

    BookingDataTableIntegrate

--- NathaniVilla.Web/Controllers/BookingController.cs
+++ NathaniVilla.Web/Controllers/BookingController.cs
@@ -9,10 +9,6 @@ using System.Security.Claims;
 
 
 
-
-
-
-
 namespace NathaniVilla.Web.Controllers
 {
     public class BookingController : Controller
@@ -22,6 +18,13 @@ namespace NathaniVilla.Web.Controllers
         {
             _unitOfWork = unitOfWork;
         }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
         [Authorize]
         public IActionResult FinalizeBooking(int villaId, DateOnly checkInDate, int nights)
         {
@@ -116,5 +119,29 @@ namespace NathaniVilla.Web.Controllers
             }
             return View(bookingId);
         }
+
+        #region API Calls
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetAll()
+        {
+            IEnumerable<Booking> objBookings;
+
+            if (User.IsInRole(SD.Role_Admin))
+            {
+                objBookings = _unitOfWork.Booking.GetAll(includeProperties: "User,Villa");
+            }
+            else
+            {
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+                objBookings = _unitOfWork.Booking.
+                    GetAll(u=>u.UserId == userId, includeProperties: "User,Villa");
+            }
+            return Json(new { data = objBookings });
+        }
+        #endregion
     }
 }
